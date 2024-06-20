583a1127b57229a4e95a248778ddb48a793b8f94
ForystekVictoria <vicky.forystek@gmail.com>
2024-06-20 14:48:37 -0300

    collection  abm

---
+++ src/controllers/collection.controller.js
@@ -0,0 +1,53 @@
+import service from '../services/collection.services.js'
+
+const getCollections = async (req, res) => {
+    try {
+        const collections = await service.getCollections()
+        res.send(collections)
+    } catch (error) {
+        console.log("Error: ", error);
+        res.send({ statusCode: 500, message: "Internal server error." });
+    }
+
+}
+
+const postCollection = async (req, res) => {
+    try {
+        const nuevaCollection = req.body
+        const collections = await service.postColecction(nuevaCollection)
+        res.send(collections)
+    } catch (error) {
+        console.log("Error: ", error);
+        res.send({ statusCode: 500, message: "Internal server error." });
+    }
+
+}
+
+const updateCollection = async (req, res) => {
+    try {
+        const {id} = req.params
+        const actualizacionCollection = req.body
+        const collections = await service.updateCollection(id, actualizacionCollection)
+        res.send(collections)
+    } catch (error) {
+        console.log("Error: ", error);
+        res.send({ statusCode: 500, message: "Internal server error." });
+    }
+
+}
+
+const deleteCollection = async (req, res) => {
+    try {
+        const {id} = req.params
+        const obras = await service.deleteCollection(id)
+        res.send(obras)
+    } catch (error) {
+        console.log("Error: ", error);
+        res.send({ statusCode: 500, message: "Internal server error." });
+    }
+
+}
+
+export default {
+    getCollections, postCollection, updateCollection, deleteCollection
+}
--- src/routes/gallery.routes.js
+++ src/routes/gallery.routes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import controller from '../controllers/gallery.controller.js'
+import collectionController from '../controllers/collection.controller.js'
 
 const router = express.Router()
 
@@ -7,5 +8,11 @@ router.get("/gallery", controller.getObras)
 router.post("/gallery", controller.postObra)
 router.put("/gallery/modificar/:id", controller.updateObra)
 router.delete("/gallery/borrar/:id", controller.deleteObra)
+
+router.get("/collection", collectionController.getCollections)
+router.post("/collection", collectionController.postCollection)
+router.put("/collection/modificar/:id", collectionController.updateCollection)
+router.delete("/collection/borrar/:id", collectionController.deleteCollection)
+
 
 export default router
