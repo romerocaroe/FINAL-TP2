478e06fa4e1f897a3a50ba99d3d0e80322ca22c3
fldeluca <frandeluca22@gmail.com>
2024-06-20 14:09:03 -0300

    Add test

---
+++ src/services/users.services.js
@@ -0,0 +1,29 @@
+//import ModelFactory from "../models/DAO/Factory.js";
+//import config from "../../config.js";
+import users from "../models/DAO/galleryMongo.model.js"
+
+//const model = ModelFactory.get(config.PERSISTENCE)
+
+const getObras = async () => {
+  const obras = await gallery.getObras()
+  return obras
+}
+
+const postObra = async (nuevaObra) => {
+  const obras = await gallery.postObra(nuevaObra)
+  return obras
+}
+
+const updateObra = async (id, actualizacionObra) => {
+  const obras = await gallery.updateObra(id, actualizacionObra)
+  return obras
+}
+
+const deleteObra = async (id) => {
+  const obras = await gallery.deleteObra(id)
+  return obras
+}
+
+export default {
+  getObras, postObra, updateObra, deleteObra
+}
