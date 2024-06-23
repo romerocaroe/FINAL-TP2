563bc442d49507ee8ffbf27f7bdf60ed2df0894f
romerocaroe <romerocaroe@gmail.com>
2024-06-23 16:48:27 -0300

    Conexión Harvard API

--- src/routes/gallery.routes.js
+++ src/routes/gallery.routes.js
@@ -5,17 +5,24 @@ import controllerUsers from '../controllers/users.controller.js'
 
 const router = express.Router()
 
+//TP2
 router.get("/obra", controllerGallery.getObras)
 router.get("/obra/:id", controllerGallery.getObraById)
 router.post("/obra", controllerGallery.postObra)
 router.put("/obra/:id", controllerGallery.updateObra)
 router.delete("/obra/:id", controllerGallery.deleteObra)
 
+//PNT2
 router.get("/usuario", controllerUsers.getUsers)
 router.get("/usuario/:username-:password", controllerUsers.getUserByUsername)
 router.post("/usuario", controllerUsers.postUser)
 router.put("/usuario/:id", controllerUsers.updateUser)
 router.delete("/usuario/:id", controllerUsers.deleteUser)
+
+/* router.patch("/usuario/coleccion/:idUsuario-:idCollection-:idObra", controllerUsers.addObraToCollection)//TODO: pasar data al body
+router.patch("/usuario/likes/:idUsuario-:idObra", controllerUsers.addObraToLike)//TODO: pasar data al body
+router.patch("/usuario/coleccion/:idUsuario-:idCollection-:idObra", collectionController.deleteObraFromCollection) //TODO:
+router.patch("/usuario/likes/:idUsuario-:idObra", collectionController.deleteObraFromLikes) //TODO: pasar data al body */
 
 router.get("/coleccion", collectionController.getCollections)
 router.get("/coleccion/:id", collectionController.getCollectionById)
